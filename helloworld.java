fd6bcb4c36ea302437864a4323dfab214b4a5133
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-10-18 14:40:01 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+package sudalai;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My Name Is Ponsudalai");
+        System.out.println("I Am Study In Edubridge");
+        System.out.println("Java Full Stack");
+        System.out.println("I Am From TamilNadu");
+	}
+
+}
