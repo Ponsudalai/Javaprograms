714fea99d4cfb942d3c7638b8f12f3039a260df4
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-10-18 15:42:49 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,41 @@
+
+package types;
+
+public class function {
+
+	public static void main(String[] args) {
+		//decleartion of variable 
+		int intval;
+		float favl;
+		double davl;
+		char cval;
+		short savl;
+		byte bval;
+		long lavl;
+		boolean boolval;
+		
+		//assign the value 
+		intval= 750;
+		
+		favl=750.3f;
+		davl=552.2;
+		cval='S';
+		lavl=5564;
+		boolval=true;
+		savl=77;
+		bval=48;
+		
+		//displying values
+		
+		System.out.println("integer value ="+intval);
+		System.out.println(" float value ="+favl);
+		System.out.println(" double value="+ davl);
+		System.out.println("character value ="+cval);
+		System.out.println("short value ="+savl);
+		System.out.println("byte value ="+bval );
+		System.out.println("long value ="+ lavl);
+		System.out.println("boolean value="+boolval);
+		
+	}
+
+}
