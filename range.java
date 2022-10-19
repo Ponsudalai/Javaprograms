aecb2e9cf4c2389026f702d77ef5f9558f94966f
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-10-19 16:14:16 +0530

    Add files via upload

---
+++ range.java
@@ -0,0 +1,43 @@
+package types;
+
+public class range {
+
+	public static void main(String[] args) {
+		
+		boolean loT=true;
+		System.out.println(loT);
+		
+
+		byte range;
+		range=127;
+		System.out.println(range);
+		
+		Short range1;
+		range1=-127;
+		System.out.println(range1);
+		
+		int range2=300000000;
+		System.out.println(range2);
+		
+		long range3=300000000l;
+		System.out.println(range3);
+		
+		double number=77.7;
+		System.out.println(number);
+
+		
+		float number1=77.7f;
+		System.out.println(number1);
+		
+		char letter='\u0041';
+		System.out.println(letter);
+		
+		char letter1=65;
+		System.out.println(letter1);  
+		
+		char letter2='A';
+		System.out.println(letter2);
+		
+	}
+
+}
