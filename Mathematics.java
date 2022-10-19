6edcd4688f4363c6a05b4f0efa19d77c2517621b
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-10-19 14:47:44 +0530

    Add files via upload

---
+++ Mathematics.java
@@ -0,0 +1,33 @@
+package types;
+
+public class Mathematics {
+
+	public static void main(String[] args) {
+		float num1,num2,ans;
+		num1=55.5f;
+		num2=22.5f;
+		
+		//addition
+		ans=num1+num2;
+		System.out.println("the addition of "+num1+" and "+ num2+" is "+ ans );
+		
+		//subration 
+		ans=num1-num2;
+		System.out.println("the subration of "+num1+" and "+ num2+" is "+ ans );
+		
+		//multiple
+		ans=num1*num2;
+		System.out.println("the multiple of "+num1+" and "+ num2+" is "+ ans );
+		
+		//divition
+		ans=num1/num2;
+		System.out.println("the division of "+num1+" and "+ num2+" is "+ ans );
+		
+		//percentage 
+		ans=num1%num2;
+		System.out.println("the percentage of "+num1+" and "+ num2+" is "+ ans );
+		
+
+	}
+
+}
