79a459ddd7febb479d07c9bd70c5836b6ec04dc6
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-10-19 14:47:27 +0530

    Add files via upload

---
+++ Angles.java
@@ -0,0 +1,38 @@
+package types;
+
+public class Angles {
+
+	public static void main(String[] args) {
+		
+		//declare  a variable
+		//data type variable name;
+		
+		float pi,r, area; //declaration
+		pi=41.2f;
+		r=2;
+		area=pi*r*r;
+		System.out.println("the arae of area" + area);
+		
+		
+		float b,h,triangle; //declaration
+		b=7;
+		h=7;
+		triangle=(b*h)/2;
+		System.out.println("the arae of triangle" + triangle);
+		
+		int width,height,rectangular;  //declaration
+		width=4;
+		height=8;
+		rectangular=width*height;
+		System.out.println("the arae of rectangular" + rectangular);
+		
+
+		int a,circle; //declaration
+		a=4;
+		circle=a*a;
+		System.out.println("the arae of circle" + circle);
+		
+
+	}
+
+}
