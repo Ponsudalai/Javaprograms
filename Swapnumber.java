58e157121d08a2a43de33fc3c3e514786e7b4a34
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-10-19 14:48:00 +0530

    Add files via upload

---
+++ Swapnumber.java
@@ -0,0 +1,25 @@
+package types;
+
+public class Swapnumber {
+
+	public static void main(String[] args) {
+		
+		
+		int a,b,c;
+		a=30;
+		b=70;
+		
+		System.out.println("before swaping of a="+a+" and b="+b);
+		
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("after swaping of a="+a+" and b="+b);
+
+		
+		
+		
+	}
+
+}
