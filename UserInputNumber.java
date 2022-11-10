a80eecc9d531dcbf65b00075c55a50db3da22982
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-11-10 17:17:48 +0530

    Add files via upload

--- UserInputNumber.java
+++ UserInputNumber.java
@@ -11,15 +11,16 @@ public class UserInputNumber {
 		ArrayList<Float>ps=new ArrayList<Float>();
 		
 		float npps;
-		Scanner ns=new Scanner(System.in);
-		System.out.println("Enter Number Of Elements");
-		int n=ns.nextInt();
-		
-		for(int i=0;i<n;i++) {
-			System.out.println("Enter The Float Value");
-			npps=ns.nextFloat();
-			ps.add(npps);
+		try (Scanner ns = new Scanner(System.in)) {
+			System.out.println("Enter Number Of Elements");
+			int n=ns.nextInt();
 			
+			for(int i=0;i<n;i++) {
+				System.out.println("Enter The Float Value");
+				npps=ns.nextFloat();
+				ps.add(npps);
+				
+			}
 		}
 		System.out.println(ps);
 			
