4ba708f3abaa36fd807f773035e7f8e7ae311f61
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-12-17 19:21:42 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,9 +1,75 @@
-package p1;
+package com.edu;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="emp_table")
 public class Employee {
-	 String name;
-	public void display() {
-		System.out.println("Hello India "+name);
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int employeeid;
+	private String employeeName;
+	private float employeeSalary;
+	@Column(name="EMail",length = 50,unique =true )
+	private String employeeEmail;
+	@Column(name="Phone",unique = true)
+	private long employeePhone;
+	public Employee() {
+		super();
+		
 	}
-
+	public Employee( String employeeName, float employeeSalary, String employeeEmail,
+			long employeePhone) {
+		super();
+		
+		this.employeeName = employeeName;
+		this.employeeSalary = employeeSalary;
+		this.employeeEmail = employeeEmail;
+		this.employeePhone = employeePhone;
+	}
+	public int getEmployeeid() {
+		return employeeid;
+	}
+	public void setEmployeeid(int employeeid) {
+		this.employeeid = employeeid;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public float getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(float employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	public String getEmployeeEmail() {
+		return employeeEmail;
+	}
+	public void setEmployeeEmail(String employeeEmail) {
+		this.employeeEmail = employeeEmail;
+	}
+	public long getEmployeePhone() {
+		return employeePhone;
+	}
+	public void setEmployeePhone(long employeePhone) {
+		this.employeePhone = employeePhone;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeid=" + employeeid + ", employeeName=" + employeeName + ", employeeSalary="
+				+ employeeSalary + ", employeeEmail=" + employeeEmail + ", employeePhone=" + employeePhone + "]";
+	}
+	
+	
+	
 }
--- MainApp.java
+++ MainApp.java
@@ -1,14 +1,61 @@
 package com.edu;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx=new ClassPathXmlApplicationContext("context.xml");
-		Printable pob=(Printable) ctx.getBean("np");
-		pob.print();
+		Configuration con=new Configuration();
+		con.configure("hibernate.cfg.xml");
+		
+		con.addAnnotatedClass(Employee.class);
+		
+					SessionFactory sessionFactory= con.buildSessionFactory();
+		
+					Session session=sessionFactory.openSession();
+	
+				Transaction transaction= session.beginTransaction();
+				
+				/*Employee ne=new Employee();
+				
+				//ne.setEmployeeName("gibbs");
+				//ne.setEmployeeSalary(19599f);
+				//ne.setEmployeeEmail("gibbs@gmail.com");
+				//ne.setEmployeePhone(9710173838l);
+				
+				Employee va=new Employee("Sivaji A",45000f,"sivaji304@gmail.com",1237654987l);
+				
+				
+				session.save(va);
+				
+				session.delete(va);
+				System.out.println("Employee"+va);
+				Employee e=session.get(Employee.class,2);
+				System.out.println(e);
+				//transaction.commit();*/
+				
+				Query query=session.createQuery("from Employee");
+				List<Employee>lit=query.list();
+				
+				Iterator<Employee> it=lit.iterator();
+				
+				System.out.println("E_Id\tE_Name\tE_Phone\tE_Email\tE_Salary");
+				while(it.hasNext()) {
+					Employee e=it.next();
+					System.out.println(e.getEmployeeid()+"\t"+e.getEmployeeName()+"\t"+e.getEmployeePhone()+"\t"+e.getEmployeeEmail()+"\t"+e.getEmployeeSalary());
+				}
+				
 	}
+	
 
 }
