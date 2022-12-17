5bb4eba2d479fb836f2aa9c891c90e37bb0099b6
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-12-17 19:13:41 +0530

    Add files via upload

---
+++ AddtionTest.java
@@ -0,0 +1,33 @@
+package com.edu;
+
+public class AddtionTest {
+
+	
+		int i,j,s;
+		int add(int i,int j) {// Addition Process
+			this.i=i;
+			this.j=j;
+			s=i+j;
+			return s;
+		}
+		
+		int sub(int i,int j) { //Subtraction Process
+			this.i=i;
+			this.j=j;
+			s=i-j;
+			return s;
+		}
+		int mul(int i,int j) { // Multiple Process
+			this.i=i;
+			this.j=j;
+			s=i*j;
+			return s;
+		}
+		int div(int i,int j) {  // Division Process
+			this.i=i;
+			this.j=j;
+			s=i/j;
+			return s;
+		}
+		
+}
