7840ea53b051b985a9f57e39d02b1b10fa234d0c
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-12-17 19:19:56 +0530

    Add files via upload

---
+++ Divition.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+public class Divition {
+	
+	int p1,p2;
+	public  Divition(int p1,int p2) {
+		this.p1=p1;
+		this.p2=p2;
+		}
+public int divitionNum()  {
+	return p1/p2;
+}
+}
