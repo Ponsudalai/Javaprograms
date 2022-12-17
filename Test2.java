10a5206ca994118865f75de732e381b306f8f684
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-12-17 19:38:34 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,16 @@
+package edu.com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test2 {
+
+	@Test
+	public void test() {
+		String s="hi";
+		System.out.println("Hello There!");
+		assertEquals("hi", s);
+	
+	}
+}
