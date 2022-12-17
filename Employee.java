3513032dfd05f89e119c5e52ef748c31cf66e79c
Ponsudalai <116068640+Ponsudalai@users.noreply.github.com>
2022-12-17 19:22:11 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,75 +1,8 @@
 package com.edu;
 
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-
-@Entity
-@Table(name="emp_table")
 public class Employee {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int employeeid;
-	private String employeeName;
-	private float employeeSalary;
-	@Column(name="EMail",length = 50,unique =true )
-	private String employeeEmail;
-	@Column(name="Phone",unique = true)
-	private long employeePhone;
-	public Employee() {
-		super();
-		
+	public void display() {
+		System.out.println("Hello Spring");
 	}
-	public Employee( String employeeName, float employeeSalary, String employeeEmail,
-			long employeePhone) {
-		super();
-		
-		this.employeeName = employeeName;
-		this.employeeSalary = employeeSalary;
-		this.employeeEmail = employeeEmail;
-		this.employeePhone = employeePhone;
-	}
-	public int getEmployeeid() {
-		return employeeid;
-	}
-	public void setEmployeeid(int employeeid) {
-		this.employeeid = employeeid;
-	}
-	public String getEmployeeName() {
-		return employeeName;
-	}
-	public void setEmployeeName(String employeeName) {
-		this.employeeName = employeeName;
-	}
-	public float getEmployeeSalary() {
-		return employeeSalary;
-	}
-	public void setEmployeeSalary(float employeeSalary) {
-		this.employeeSalary = employeeSalary;
-	}
-	public String getEmployeeEmail() {
-		return employeeEmail;
-	}
-	public void setEmployeeEmail(String employeeEmail) {
-		this.employeeEmail = employeeEmail;
-	}
-	public long getEmployeePhone() {
-		return employeePhone;
-	}
-	public void setEmployeePhone(long employeePhone) {
-		this.employeePhone = employeePhone;
-	}
-	@Override
-	public String toString() {
-		return "Employee [employeeid=" + employeeid + ", employeeName=" + employeeName + ", employeeSalary="
-				+ employeeSalary + ", employeeEmail=" + employeeEmail + ", employeePhone=" + employeePhone + "]";
-	}
-	
-	
-	
+
 }
